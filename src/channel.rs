7f46f9098eb6c2a805aee26ff97f389180bd11a9
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-05-28 12:23:39 -1000

    rename channel fields

--- src/channel.rs
+++ src/channel.rs
@@ -3,21 +3,21 @@ use std::sync::mpsc::{self, Receiver, Sender};
 use crate::error::Result;
 
 pub struct Channel<T> {
-  pub send: Sender<T>,
-  pub recv: Receiver<T>,
+  pub sender: Sender<T>,
+  pub receiver: Receiver<T>,
 }
 
 impl<T> Channel<T> {
   pub fn new() -> Self {
-    let (send, recv) = mpsc::channel();
-    Self { send, recv }
+    let (sender, receiver) = mpsc::channel();
+    Self { sender, receiver }
   }
 
   pub fn poll(&self) -> Result<T> {
-    Ok(self.recv.recv()?)
+    Ok(self.receiver.recv()?)
   }
 
   pub fn sender(&self) -> Sender<T> {
-    self.send.clone()
+    self.sender.clone()
   }
 }
